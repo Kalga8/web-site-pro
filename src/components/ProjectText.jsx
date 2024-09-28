d25e3d7ad561802f91d6a33bc7af69ddfbbc9b47
Justine Launay <jlaunay8@outlook.fr>
2024-09-28 12:46:55 +0200

    test

---
+++ src/components/ProjectText.jsx
@@ -0,0 +1,10 @@
+function ProjectText() {
+    <div className="textDescription">
+        <h3> DESCRIPTION </h3>
+            <p></p>
+        <h4> DIFFICULTÉS</h4>
+            <p></p>
+    </div>
+}
+
+export default ProjectText
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,6 +1,7 @@
 import { Navigate, useParams } from "react-router-dom";
 import Carousel from "../components/Carousel";
 import projectData from "../data/projects.json";
+import ProjectText from "../components/ProjectText";
 
 function Project() {
     const { id } = useParams(); /*Hook récupérant l'id depuis URL*/
@@ -15,7 +16,13 @@ function Project() {
   
     return (
         <div className="projectContainer">
-            <Carousel pictures={selectedProjects.pictures} />
+            <div className="ProjectTextBlock">
+                <ProjectText />
+            </div>
+
+            <div className="CarouselBlock">
+                <Carousel pictures={selectedProjects.pictures} />
+            </div>
         </div>
     )
 }
