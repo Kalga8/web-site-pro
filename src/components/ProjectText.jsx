43053305d5336d56288c86c02bff18dafc617d2c
Pierre <pierre.sourice38@gmail.com>
2025-05-28 22:15:45 +0200

    Mise à jour de ProjectText.jsx

--- src/components/ProjectText.jsx
+++ src/components/ProjectText.jsx
@@ -4,11 +4,11 @@ function ProjectText({id, description, competences, difficulties}) {
         <h3> DESCRIPTION </h3>
             <p> {description} </p>
 
-        <h4> COMPÉTENCES </h4>
-            <p> {competences} </p>
+        {/*<h4> COMPÉTENCES </h4>*/}
+        {/*    <p> {competences} </p>*/}
 
-        <h4> DIFFICULTÉS </h4>
-            <p> {difficulties} </p>
+        {/*<h4> DIFFICULTÉS </h4>*/}
+        {/*    <p> {difficulties} </p>*/}
     </div>
     )
 }
